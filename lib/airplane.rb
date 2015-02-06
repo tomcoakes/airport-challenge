472a9c003872a53aa1dc8729531d1ea45fa50a27
Tom Coakes <tomcoakes@gmail.com>
2015-02-06 12:58:01 +0000

    Added land_at(airport) and take_off methods for Airplane class.

--- lib/airplane.rb
+++ lib/airplane.rb
@@ -8,6 +8,10 @@ class Airplane
     @flying_status
   end
 
+  def flying=(value)
+    @flying_status = value
+  end
+
   def stop_flying
     @flying_status = false
   end
@@ -15,6 +19,16 @@ class Airplane
   def land_at(airport)
     airport.landing_plane(self)
     stop_flying
+    @location = airport
+  end
+
+  def location
+    @location
+  end
+
+  def take_off
+    location.leaving_plane
+    self.flying = true
   end
 
 end
--- spec/airplane_spec.rb
+++ spec/airplane_spec.rb
@@ -3,7 +3,9 @@ require 'airplane'
 describe Airplane do
  
   let(:boeing) {Airplane.new}
-  let(:stansted) {double :stansted}
+  let(:empty_airport) {double :empty_airport, gates: []}
+  let(:airport_with_one_plane) {double :airport_with_one_plane, gates: [boeing]}
+
 
   it "should be able to fly" do
     expect(boeing).to be_flying
@@ -15,8 +17,29 @@ describe Airplane do
   end
 
   it "should be able to land in an airport" do
-    expect(stansted).to receive(:landing_plane).with(boeing)
-    boeing.land_at(stansted)
+    expect(empty_airport).to receive(:landing_plane).with(boeing)
+    boeing.land_at(empty_airport)
+  end
+
+  it "should know what airport it is in" do
+    allow(empty_airport).to receive(:landing_plane)
+    boeing.land_at(empty_airport)
+    expect(boeing.location).to eq(empty_airport)
+  end
+
+  it "should be able to take off" do
+    expect(empty_airport).to receive(:landing_plane)
+    boeing.land_at(empty_airport)
+    expect(empty_airport).to receive(:leaving_plane)
+    boeing.take_off
+  end
+
+  it "should be flying once it's taken off" do
+    expect(empty_airport).to receive(:landing_plane)
+    boeing.land_at(empty_airport)
+    expect(empty_airport).to receive(:leaving_plane)
+    boeing.take_off
+    expect(boeing).to be_flying
   end
 
 end
